48d2a301b619e0ca35a427133bf7f3c27af1b18b
Yajat Gupta <85209180+yajat-payglocal@users.noreply.github.com>
2022-11-07 16:46:52 +0530

    added plugin for PHP 7.2-7.4

--- vendor/spomky-labs/aes-key-wrap/src/A256KW.php
+++ vendor/spomky-labs/aes-key-wrap/src/A256KW.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2018 Spomky-Labs
+ * Copyright (c) 2014-2020 Spomky-Labs
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
@@ -17,9 +17,6 @@ final class A256KW
 {
     use AESKW;
 
-    /**
-     * {@inheritdoc}
-     */
     protected static function getMethod(): string
     {
         return 'aes-256-ecb';
